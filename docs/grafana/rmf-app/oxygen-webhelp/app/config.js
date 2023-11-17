bd768da6e9f87f23c43e306fee7b2731e71fe5b5
Dmitry Nikolaev <39479675+DNikolaevAtRocket@users.noreply.github.com>
2023-11-17 17:16:15 +0100

    Update docs (#6)
    Signed-off-by: Dmitry Nikolaev <dnikolaev@rocketsoftware.com>

--- docs/grafana/rmf-app/oxygen-webhelp/app/config.js
+++ docs/grafana/rmf-app/oxygen-webhelp/app/config.js
@@ -114,7 +114,7 @@ define(function() {
         	
             if(id === 'index-1' || id === 'index-2' || id === 'index-3' || id === 'stopwords' 
             ||id === 'htmlFileInfoList' || id === 'keywords') {
-                args = '20231116232525';
+                args = '20231117190637';
         	} 
 	
 	        return (url.indexOf('?') === -1 ? '?' : '&') + args;
