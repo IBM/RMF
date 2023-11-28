c70005384216e94943a9e09ed59f41457578faec
Dmitry Nikolaev <39479675+DNikolaevAtRocket@users.noreply.github.com>
2023-11-28 16:58:35 +0100

    v1.0.1 (#10)
    * Update docs
    
    Signed-off-by: Dmitry Nikolaev <dnikolaev@rocketsoftware.com>
    
    * v.1x:Golang and JS Security Issues fixed
    
    v.1x:Golang and JS Security Issues fixed
    
    * Doc update v1.0.1 (#9)
    
    Signed-off-by: Dmitry Nikolaev <dnikolaev@rocketsoftware.com>
    
    ---------
    
    Signed-off-by: Dmitry Nikolaev <dnikolaev@rocketsoftware.com>
    Co-authored-by: Sivasubramanian R <siva.uk@outlook.com>
    Co-authored-by: Guruprasad Gowda <43952875+ggowdarocket@users.noreply.github.com>

--- docs/grafana/rmf-app/oxygen-webhelp/app/config.js
+++ docs/grafana/rmf-app/oxygen-webhelp/app/config.js
@@ -114,7 +114,7 @@ define(function() {
         	
             if(id === 'index-1' || id === 'index-2' || id === 'index-3' || id === 'stopwords' 
             ||id === 'htmlFileInfoList' || id === 'keywords') {
-                args = '20231117190637';
+                args = '20231128201010';
         	} 
 	
 	        return (url.indexOf('?') === -1 ? '?' : '&') + args;
