ef57c2b3feb4ecab176959a4201e2a9e87ea683a
Ramanath Shanbhag <rshanbhag@rocketsoftware.com>
2024-02-29 20:12:41 +0530

    Updated changemod and release notes topic
    Signed-off-by: Ramanath Shanbhag <rshanbhag@rocketsoftware.com>

--- docs/grafana/rmf-app/oxygen-webhelp/app/config.js
+++ docs/grafana/rmf-app/oxygen-webhelp/app/config.js
@@ -114,7 +114,7 @@ define(function() {
         	
             if(id === 'index-1' || id === 'index-2' || id === 'index-3' || id === 'stopwords' 
             ||id === 'htmlFileInfoList' || id === 'keywords') {
-                args = '20240207202108';
+                args = '20240229200611';
         	} 
 	
 	        return (url.indexOf('?') === -1 ? '?' : '&') + args;
