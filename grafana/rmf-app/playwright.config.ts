2bfa7cef3c3558644aeef9d2d10b2cce6c0cd671
Dmitry Nikolaev <dnikolaev@rocketsoftware.com>
2024-05-28 11:54:03 +0200

    Initial e2e tests
    Signed-off-by: Dmitry Nikolaev <dnikolaev@rocketsoftware.com>

---
+++ grafana/rmf-app/playwright.config.ts
@@ -0,0 +1,50 @@
+import { defineConfig, devices } from '@playwright/test';
+
+/**
+ * See https://playwright.dev/docs/test-configuration.
+ */
+export default defineConfig({
+  testDir: './tests',
+  /* Run tests in files in parallel */
+  fullyParallel: true,
+  /* Fail the build on CI if you accidentally left test.only in the source code. */
+  forbidOnly: !!process.env.CI,
+  /* Retry on CI only */
+  retries: process.env.CI ? 2 : 0,
+  /* Opt out of parallel tests on CI. */
+  workers: process.env.CI ? 1 : undefined,
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  reporter: 'list',
+
+  /*
+  Shared settings for all the projects below.
+  See https://playwright.dev/docs/api/class-testoptions.
+  */
+  use: {
+    baseURL: 'http://127.0.0.1:3000',
+    trace: 'on-first-retry',
+    headless: true,
+  },
+
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+
+    {
+      name: 'edge',
+      use: { ...devices['Desktop Edge'] },
+    },
+
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
+  ],
+});
