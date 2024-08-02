c7ed62787392321de5ad4add07f44c3751f8cd58
Ramanath Shanbhag <rshanbhag@rocketsoftware.com>
2024-08-02 16:56:12 +0530

    v105 doc updates
    Signed-off-by: Ramanath Shanbhag <rshanbhag@rocketsoftware.com>

--- docs/grafana/rmf-app/oxygen-webhelp/app/search/index/keywords.js
+++ docs/grafana/rmf-app/oxygen-webhelp/app/search/index/keywords.js
@@ -1,4 +1,4 @@
-define(function() {var keywords=[{w:"Accessing",p:["p0","p13"]},{w:"the",p:["p0","p6","p7","p11","p17"]},{w:"Grafana",p:["p0","p6","p8","p9","p11","p13","p17","p19"]},{w:"dashboard",p:["p0","p5"]},{w:"Introduction",p:["p1"]},{w:"to",p:["p1","p2"]},{w:"Alerts",p:["p1"]},{w:"Applying",p:["p2"]},{w:"visualization",p:["p2","p19"]},{w:"RMF",p:["p2","p3","p5","p11","p14","p17","p18","p19"]},{w:"data",p:["p2","p3","p10"]},{w:"Creating",p:["p3"]},{w:"sources",p:["p3"]},{w:"Context",p:["p4"]},{w:"Sensitive",p:["p4"]},{w:"Help",p:["p4"]},{w:"master",p:["p5"]},{w:"Defining",p:["p6"]},{w:"server",p:["p6"]},{w:"Error",p:["p7"]},{w:"reporting",p:["p7"]},{w:"in",p:["p7","p19"]},{w:"plugin",p:["p7","p11","p12","p17"]},{w:"configuration",p:["p8","p12"]},{w:"parameters",p:["p8"]},{w:"through",p:["p9"]},{w:"IBM",p:["p9","p14"]},{w:"z/OS",p:["p9","p11","p17"]},{w:"Management",p:["p9"]},{w:"Facility",p:["p9"]},{w:"Historical",p:["p10"]},{w:"collection",p:["p10"]},{w:"Installing",p:["p11"]},{w:"for",p:["p11","p13","p17"]},{w:"on",p:["p11","p13","p17"]},{w:"Prerequisites",p:["p13"]},{w:"dashboards",p:["p13"]},{w:"z/OSMF",p:["p13"]},{w:"query",p:["p14","p18"]},{w:"languages",p:["p14"]},{w:"Release",p:["p15"]},{w:"notes",p:["p15"]},{w:"Troubleshooting",p:["p16"]},{w:"issues",p:["p16"]},{w:"Upgrading",p:["p17"]},{w:"Variable",p:["p18"]},{w:"syntax",p:["p18"]},{w:"of",p:["p19"]},{w:"Monitor",p:["p19"]},{w:"III",p:["p19"]},{w:"metrics",p:["p19"]}];
+define(function() {var keywords=[{w:"Accessing",p:["p0","p12"]},{w:"the",p:["p0","p6","p7","p11","p16"]},{w:"Grafana",p:["p0","p6","p8","p9","p11","p12","p16","p18"]},{w:"dashboard",p:["p0","p5"]},{w:"Introduction",p:["p1"]},{w:"to",p:["p1","p2"]},{w:"Alerts",p:["p1"]},{w:"Applying",p:["p2"]},{w:"visualization",p:["p2","p18"]},{w:"RMF",p:["p2","p3","p5","p11","p13","p16","p17","p18"]},{w:"data",p:["p2","p3","p10"]},{w:"Creating",p:["p3"]},{w:"sources",p:["p3"]},{w:"Context",p:["p4"]},{w:"Sensitive",p:["p4"]},{w:"Help",p:["p4"]},{w:"master",p:["p5"]},{w:"Defining",p:["p6"]},{w:"server",p:["p6"]},{w:"Error",p:["p7"]},{w:"reporting",p:["p7"]},{w:"in",p:["p7","p18"]},{w:"plugin",p:["p7","p11","p16"]},{w:"configuration",p:["p8"]},{w:"parameters",p:["p8"]},{w:"through",p:["p9"]},{w:"IBM",p:["p9","p13"]},{w:"z/OS",p:["p9","p11","p16"]},{w:"Management",p:["p9"]},{w:"Facility",p:["p9"]},{w:"Historical",p:["p10"]},{w:"collection",p:["p10"]},{w:"Installing",p:["p11"]},{w:"for",p:["p11","p12","p16"]},{w:"on",p:["p11","p12","p16"]},{w:"Prerequisites",p:["p12"]},{w:"dashboards",p:["p12"]},{w:"z/OSMF",p:["p12"]},{w:"query",p:["p13","p17"]},{w:"languages",p:["p13"]},{w:"Release",p:["p14"]},{w:"notes",p:["p14"]},{w:"Troubleshooting",p:["p15"]},{w:"issues",p:["p15"]},{w:"Upgrading",p:["p16"]},{w:"Variable",p:["p17"]},{w:"syntax",p:["p17"]},{w:"of",p:["p18"]},{w:"Monitor",p:["p18"]},{w:"III",p:["p18"]},{w:"metrics",p:["p18"]}];
 var ph={};
 ph["p0"]=[0, 1, 2, 3];
 ph["p1"]=[4, 5, 6];
@@ -11,15 +11,14 @@ ph["p7"]=[19, 20, 21, 1, 22];
 ph["p8"]=[2, 23, 24];
 ph["p9"]=[2, 25, 26, 27, 28, 29];
 ph["p10"]=[30, 10, 31];
-ph["p12"]=[22, 23];
+ph["p12"]=[35, 33, 0, 2, 36, 34, 37];
 ph["p11"]=[32, 1, 9, 33, 27, 22, 34, 2];
-ph["p14"]=[26, 9, 38, 39];
-ph["p13"]=[35, 33, 0, 2, 36, 34, 37];
-ph["p16"]=[42, 43];
-ph["p15"]=[40, 41];
-ph["p18"]=[9, 45, 38, 46];
-ph["p17"]=[44, 1, 9, 33, 27, 22, 34, 2];
-ph["p19"]=[8, 47, 9, 48, 49, 50, 21, 2];
+ph["p14"]=[40, 41];
+ph["p13"]=[26, 9, 38, 39];
+ph["p16"]=[44, 1, 9, 33, 27, 22, 34, 2];
+ph["p15"]=[42, 43];
+ph["p18"]=[8, 47, 9, 48, 49, 50, 21, 2];
+ph["p17"]=[9, 45, 38, 46];
      return {
          keywords: keywords,
          ph: ph
